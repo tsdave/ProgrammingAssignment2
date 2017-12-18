4547af1be88c0526bb06de481473ce0cd598b5ad
tsd <trilochandave@gmail.com>
2017-12-18 20:52:43 +1100

    Week 3 Programing Assignment - version 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix function creates a special matrix object that can cache its inverse.
+# it will take coerced data and number of rows and columns as input
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(data,nrow,ncol) {  
+  x <- matrix(data,nrow,ncol)                    # initialise object
+  inv <- NULL                                    # initalise object
+  setmatrix <-function(y,nrow,ncol) {            # set matrix
+    x <<- y                                        # set matrix to take value for y from glbal enviornment object x
+    inv <<- NULL                                   # set inverse matrix to null so that it does not take worng value from cache
+  }
+  
+  getmatrix <- function() x                      # create function to get matrix
+  setinv <- function(solve) inv <<- solve        # create function to set inverse matrix
+  getinv <- function() inv                       # create function to get inverse function value
+  list(setmatrix=setmatrix,getmatrix=getmatrix,setinv=setinv,getinv=getinv) # create new object by returning list
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# cacheSolve function will create inverse matrix 
+# it will check if Inverse matrix already exists in cache 
+# this function will take makeCacheMatrix as input
+cacheSolve <- function(x,...) {
+  inv <- x$getinv()
+  if(!is.null(inv)) {                   # check if inverse matrix exist in cache
+    message("getting cached data")
+    return(inv)                           # return inverse matrix if exists in cache
+  }
+  data <- x$getmatrix()                 # if inverse matrix does not exist in cache
+  inv <- solve(data)                    # solve for inverse matrix
+  x$setinv(inv)                         # set inverse matrix in cache
+  inv                                   # return inverse matrix
 }
+
+# functions tested with following data
+# makeCacheMatrix c(.5.-25,-1,.75)
+# inverse matrix c(6,2,8,4)
